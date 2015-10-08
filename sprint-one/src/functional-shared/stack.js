04e2222160b9754b59106f1add28f8e5849f1df8
nissaw <nissa.wollum@gmail.com>
2015-10-08 16:15:34 -0700

    Functional Shared Stack Implemeted

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = { count: 0 };
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+    this.count += 1;
+    this[this.count] = value;
+  },
 
+  pop: function() {
+    var result = this[this.count];
+    delete this[this.count];
+    this.count -=1;
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return result;
+  },
+
+  size: function() {
+    return this.count;
+  }
+};
 
