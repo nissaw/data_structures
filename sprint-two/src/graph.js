e88aa5b5806e97c67e794d031237db025bb80d20
nissaw <nissa.wollum@gmail.com>
2015-10-09 15:42:40 -0700

    Graph first three tests pass

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,22 +3,58 @@
 // ###Graph Solution
 
 // Instantiate a new graph
-var Graph = function(){
+var Graph = function (){
+  //var currentID = 0;
+  this.nodes = [];
 };
 
-// ------------------------
+var Node = function(value) {
+  this.value = value,
+  this.connections = [];
+  //this.id;
+}
+// var myGraph = new Graph();
+// var kittens  = new Node('somevalue');
+  // myNode should look like--
+  /* 
+  { value: 'somevalue',
+    connections: [],
+    id: 0,
+  }
+  */
+// myGraph.addNode(myNode);
+
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
+  this.nodes.push(node);
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
+  var doesContain = false;
+  _.each(this.nodes, function(item) {
+    if ( item === node ) { // item.value?
+      doesContain = true;
+    }
+  });
+  return doesContain;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node){
+  var removed;
+  var index;
+  _.each(this.nodes, function(item, i) {
+    if ( item === node ) {
+      removed = node 
+      index = i;
+    }
+  })
+  this.nodes.splice(index, 1)[0];
+
+  return removed;
 };
 
 // ------------------------
